b27626dc08eeae5fac3f6beef9666986980d7e6f
Ian McDowell <me@ianmcdowell.net>
2017-12-28 20:49:44 -0800

    Initial commit

---
+++ Framework/ssh2.h
@@ -0,0 +1,19 @@
+//
+//  ssh2.h
+//  ssh2
+//
+//  Created by Ian McDowell on 12/28/17.
+//  Copyright © 2017 Ian McDowell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ssh2.
+FOUNDATION_EXPORT double ssh2VersionNumber;
+
+//! Project version string for ssh2.
+FOUNDATION_EXPORT const unsigned char ssh2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ssh2/PublicHeader.h>
+
+
