40e51140ee1ee35396eee1c8ed54ac08a1b94b28
Ian McDowell <me@ianmcdowell.net>
2018-02-09 11:10:52 -0800

    Release 2/8/2018

--- Framework/ssh2.h
+++
@@ -1,4 +0,0 @@
-
-#import <ssh2/libssh2.h>
-#import <ssh2/libssh2_sftp.h>
-#import <ssh2/libssh2_publickey.h>
---
+++ Tests/ssh2_tests.m
@@ -0,0 +1,33 @@
+//
+//  tests.m
+//  tests
+//
+//  Created by Ian McDowell on 2/9/18.
+//  Copyright © 2018 Ian McDowell. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@import ssh2;
+#import <ssh2/libssh2.h>
+
+@interface SSH2Tests : XCTestCase
+
+@end
+
+@implementation SSH2Tests
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)test {
+
+}
+
+
+@end
